22dd43387022a85477087a4b87e106c6789798fb
ilominar <85649993+ilominar@users.noreply.github.com>
2024-01-03 21:18:09 -0800

    Update WYPTBuilder.cs
    Cleanup.

--- JAFDTC/Models/FA18C/Upload/WYPTBuilder.cs
+++ JAFDTC/Models/FA18C/Upload/WYPTBuilder.cs
@@ -2,7 +2,7 @@
 //
 // WYPTBuilder.cs -- fa-18c waypoint command builder
 //
-// Copyright(C) 2023 ilominar/raven
+// Copyright(C) 2023-2024 ilominar/raven
 //
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
 // Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
@@ -17,10 +17,9 @@
 //
 // ********************************************************************************************************************
 
-using JAFDTC.Models.FA18C.WYPT;
 using JAFDTC.Models.DCS;
+using JAFDTC.Models.FA18C.WYPT;
 using System.Text;
-using JAFDTC.Models.Base;
 using JAFDTC.Utilities;
 
 namespace JAFDTC.Models.FA18C.Upload
@@ -45,9 +44,8 @@ namespace JAFDTC.Models.FA18C.Upload
         // ------------------------------------------------------------------------------------------------------------
 
         /// <summary>
-        /// configure radio system (com1/com2 uhf/vhf radios) via the icp/ded according to the non-default programming
-        /// settings (this function is safe to call with a configuration with default settings: defaults are skipped as
-        /// necessary).
+        /// configure steerpoint system via the ufc/rmfd according to the non-default programming settings (this
+        /// function is safe to call with a configuration with default settings: defaults are skipped as necessary).
         /// <summary>
         public override void Build()
         {
@@ -76,6 +74,8 @@ namespace JAFDTC.Models.FA18C.Upload
 
                     if (wypt.IsValid)
                     {
+                        // NOTE: coords are zero-filled in the ui, back that out here.
+
                         AppendCommand(ufc.GetCommand("Opt1"));
                         AppendCommand(Wait());
                         AppendCommand(Build2864Coordinate(ufc, Coord.RemoveLLDegZeroFill(wypt.LatUI)));    // DDM
@@ -119,138 +119,5 @@ namespace JAFDTC.Models.FA18C.Upload
                 SelectWp0(rmfd, i + 1);
             }
         }
-
-#if NOPE
-
-        private void selectWp0(Device rmfd, int i)
-        {
-            if (i < 140) // It might not notice on the first pass, so we go around once more
-            {
-                AppendCommand(StartCondition("NotAtWp0"));
-                AppendCommand(rmfd.GetCommand("OSB-13"));
-                AppendCommand(EndCondition("NotAtWp0"));
-                selectWp0(rmfd, i + 1);
-            }
-        }
-        public override void Build()
-        {
-            var wpts = _cfg.Waypoints.Waypoints;
-            var wptStart = _cfg.Waypoints.SteerpointStart;
-            var wptEnd = wptStart + wpts.Count;
-
-            if (wpts.Count == 0)
-            {
-                return;
-            }
-
-            var wptDiff = wptEnd - wptStart;
-
-            var ufc = _aircraft.GetDevice("UFC");
-            var rmfd = _aircraft.GetDevice("RMFD");
-            AppendCommand(rmfd.GetCommand("OSB-18")); // MENU
-            AppendCommand(rmfd.GetCommand("OSB-18")); // MENU
-            AppendCommand(rmfd.GetCommand("OSB-02")); // HSI
-
-            AppendCommand(rmfd.GetCommand("OSB-10")); // DATA
-            AppendCommand(rmfd.GetCommand("OSB-07")); // WYPT
-            AppendCommand(rmfd.GetCommand("OSB-05")); // UFC
-
-            selectWp0(rmfd, 0);
-            for (var i = 0; i < wptStart; i++)
-            {
-                AppendCommand(rmfd.GetCommand("OSB-12"));
-            }
-
-            for (var i = 0; i < wptDiff; i++)
-            {
-                Waypoint wpt;
-                wpt = wpts[i];
-
-                if (wpt.Blank)
-                {
-                    continue;
-                }
-
-                AppendCommand(ufc.GetCommand("Opt1"));
-                AppendCommand(Wait());
-                AppendCommand(BuildCoordinate(ufc, wpt.Latitude));
-                AppendCommand(ufc.GetCommand("ENT"));
-                AppendCommand(WaitLong());
-
-                AppendCommand(BuildCoordinate(ufc, wpt.Longitude));
-                AppendCommand(ufc.GetCommand("ENT"));
-                AppendCommand(WaitLong());
-
-                AppendCommand(ufc.GetCommand("Opt3"));
-                AppendCommand(ufc.GetCommand("Opt1"));
-                AppendCommand(BuildDigits(ufc, wpt.Elevation.ToString()));
-                AppendCommand(ufc.GetCommand("ENT"));
-                AppendCommand(Wait());
-
-                AppendCommand(rmfd.GetCommand("OSB-12")); // Next Waypoint
-            }
-            for (var i = 0; i < wptDiff; i++)
-            {
-                AppendCommand(rmfd.GetCommand("OSB-13")); // Prev Waypoint
-            }
-
-            AppendCommand(rmfd.GetCommand("OSB-18"));
-            AppendCommand(rmfd.GetCommand("OSB-18"));
-            AppendCommand(rmfd.GetCommand("OSB-15"));
-        }
-
-        private string BuildCoordinatxxxe(Device ufc, string coord)
-        {
-            var sb = new StringBuilder();
-
-            var latStr = RemoveSeparators(coord.Replace(" ", ""));
-            var i = 0;
-            var lon = false;
-            var longLon = false;
-
-            foreach (var c in latStr.ToCharArray())
-            {
-                if (c == 'N')
-                {
-                    sb.Append(ufc.GetCommand("2"));
-                    i = 0;
-                }
-                else if (c == 'S')
-                {
-                    sb.Append(ufc.GetCommand("8"));
-                    i = 0;
-                }
-                else if (c == 'E')
-                {
-                    sb.Append(ufc.GetCommand("6"));
-                    i = 0;
-                    lon = true;
-                }
-                else if (c == 'W')
-                {
-                    sb.Append(ufc.GetCommand("4"));
-                    i = 0;
-                    lon = true;
-                }
-                else
-                {
-                    if (i <= 5 || (i <= 6 && longLon))
-                    {
-                        if (!(i == 0 && c == '0' && lon))
-                        {
-                            if (i == 0 && c == '1' && lon) longLon = true;
-
-                            sb.Append(ufc.GetCommand(c.ToString()));
-                            i++;
-                            lon = false;
-                        }
-
-                    }
-                }
-            }
-
-            return sb.ToString();
-        }
-#endif
     }
 }
