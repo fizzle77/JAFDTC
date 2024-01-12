594b4cdd26c241959040d7fb5917b1fa25d062b9
ilominar <85649993+ilominar@users.noreply.github.com>
2024-01-11 22:20:18 -0800

    Version Updated
    Updated version to B.18.

--- JAFDTC/AppTypes.cs
+++ JAFDTC/AppTypes.cs
@@ -65,8 +65,8 @@ namespace JAFDTC
 
         public static readonly JsonSerializerOptions JSONOptions = new() { WriteIndented = true };
 
-        public const string VersionJAFDTC = "v1.0.0-B.17";              // current version
+        public const string VersionJAFDTC = "v1.0.0-B.18";              // current version
 
-        public const string BuildJAFDTC = "version 1.0.0-B.17 of 28-DEC-23 (build 6.62607015)";
+        public const string BuildJAFDTC = "version 1.0.0-B.18 of 12-JAN-24 (build 299792458)";
     }
 }
