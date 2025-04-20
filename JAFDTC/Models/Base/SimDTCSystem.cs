cf18e22f4c354ed1f96ebc69a76d197afc081a77
ilominar <85649993+ilominar@users.noreply.github.com>
2025-04-20 11:46:23 -0700

    Create SimDTCSystem.cs
    Added general DTC system to support DCS DTC output.

---
+++ JAFDTC/Models/Base/SimDTCSystem.cs
@@ -0,0 +1,137 @@
+﻿// ********************************************************************************************************************
+//
+// SimDTCSystem.cs : simulator dtc system
+//
+// Copyright(C) 2025 ilominar/raven
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+// Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program.  If not, see
+// <https://www.gnu.org/licenses/>.
+//
+// ********************************************************************************************************************
+
+using JAFDTC.Utilities;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json.Serialization;
+
+namespace JAFDTC.Models.Base
+{
+    /// <summary>
+    /// class to capture the settings of the DTC "system". this serves as a helper to integrate JAFDTC system settings
+    /// with settings from the built-in DTC in DCS. this system is airframe-agnostic
+    /// </summary>
+    public class SimDTCSystem : SystemBase
+    {
+        public const string SystemTag = "JAFDTC:Generic:DTC";
+
+        // ------------------------------------------------------------------------------------------------------------
+        //
+        // properties
+        //
+        // ------------------------------------------------------------------------------------------------------------
+
+        // ---- following properties do not post change or validation events.
+
+        private string _template;
+        public string Template
+        {
+            get => _template;
+            set => SetProperty(ref _template, value, null);
+        }
+
+        private string _outputPath;
+        public string OutputPath
+        {
+            get => _outputPath;
+            set => SetProperty(ref _outputPath, value, null);
+        }
+
+        public ObservableCollection<string> MergedSystemTags { get; set; }
+
+        private string _enableLoad;
+        public string EnableLoad
+        {
+            get => _enableLoad;
+            set => ValidateAndSetBoolProp(value, ref _enableLoad);
+        }
+
+        /// <summary>
+        /// returns true if the instance indicates a default setup, false otherwise.
+        /// </summary>
+        [JsonIgnore]
+        public override bool IsDefault => ((Template.Length == 0) &&
+                                           (OutputPath.Length == 0) &&
+                                           (MergedSystemTags.Count == 0) &&
+                                           (EnableLoadValue == false));
+
+        // ---- following accessors get the current value (default or non-default) for various properties
+
+        [JsonIgnore]
+        public bool EnableLoadValue => !string.IsNullOrEmpty(EnableLoad) && bool.Parse(EnableLoad);
+
+        // ------------------------------------------------------------------------------------------------------------
+        //
+        // construction
+        //
+        // ------------------------------------------------------------------------------------------------------------
+
+        public SimDTCSystem()
+        {
+            Template = "";
+            OutputPath = "";
+            EnableLoad = false.ToString();
+            MergedSystemTags = new ObservableCollection<string>();
+        }
+
+        public SimDTCSystem(SimDTCSystem other)
+        {
+            Template = new(other.Template);
+            OutputPath = new(other.OutputPath);
+            EnableLoad = new(other.EnableLoad);
+            MergedSystemTags = new ObservableCollection<string>();
+        }
+
+        public virtual object Clone() => new SimDTCSystem(this);
+
+        // ------------------------------------------------------------------------------------------------------------
+        //
+        // methods
+        //
+        // ------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// validates the Template and OutputPath fields are valid. if not, they are reset to default values.
+        /// </summary>
+        public void ValidateForAirframe(AirframeTypes airframe)
+        {
+            if (!FileManager.IsValidDTCTemplate(airframe, Template))
+                Template = "";
+            if (!string.IsNullOrEmpty(OutputPath))
+            {
+                string path = Path.GetDirectoryName(OutputPath);
+                if (!Directory.Exists(path))
+                    OutputPath = "";
+            }
+        }
+
+        /// <summary>
+        /// reset the instance to defaults.
+        /// </summary>
+        public override void Reset()
+        {
+            Template = "";
+            OutputPath = "";
+            EnableLoad = false.ToString();
+            MergedSystemTags.Clear();
+        }
+    }
+}
