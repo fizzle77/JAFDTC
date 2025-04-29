04abf981acccd4c9169285da2101086129a25fe5
ilominar <85649993+ilominar@users.noreply.github.com>
2025-04-28 19:15:26 -0700

    Update CommonFunctions.lua
    Refactored common functions, rationalized function names (with trampolines to deprecate the old names when ready). Documentation updates.

--- JAFDTC/DCS/JAFDTC/CommonFunctions.lua
+++ JAFDTC/DCS/JAFDTC/CommonFunctions.lua
@@ -4,7 +4,7 @@
 CommonFunctions.lua -- jafdtc common functions
 
 Copyright(C) 2021-2023 the-paid-actor & dcs-dtc contributors
-Copyright(C) 2023-2024 ilominar/raven
+Copyright(C) 2023-2025 ilominar/raven
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
 Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
@@ -29,19 +29,55 @@ function JAFDTC_Log(str)
     end
 end
 
-function JAFDTC_ParseDisplay(indicator_id)  -- Thanks to [FSF]Ian code
-    local t = { }
-    local li = list_indication(indicator_id)
-    local m = li:gmatch("-----------------------------------------\n([^\n]*)\n([^\n]*)\n")
-    while true do
-        local name, value = m()
-        if not name then
-            break
-        elseif string.len(name) > 0 then
-            t[name] = value
-        end
+function JAFDTC_GetPlayerAircraftType()
+    local data = LoGetSelfData();
+    if data then
+        -- TODO: do a table lookup here?
+        local model = string.upper(data["Name"])
+        if model == "AV8BNA" then return "AV8B" end
+        if model == "A-10C_2" then return "A10C" end
+        if model == "F-14A-135-GR" then return "F14AB" end
+        if model == "F-14B" then return "F14AB" end
+        if model == "F-15ESE" then return "F15E" end
+        if model == "F-16C_50" then return "F16CM" end
+        if model == "FA-18C_HORNET" then return "FA18C" end
+        if model == "M-2000C" then return "M2000C" end
+        return model;
     end
-    return t
+    return "Unknown"
+end
+
+function JAFDTC_SerializeObj(val, name, skipnewlines, depth)
+    skipnewlines = skipnewlines or false
+    depth = depth or 0
+
+    local tmp = string.rep(" ", depth)
+
+    if name then tmp = tmp .. "<" .. name .. "> = " end
+
+    if type(val) == "table" then
+        tmp = tmp .. "{" .. (not skipnewlines and "\n" or "")
+
+        for k, v in pairs(val) do
+            tmp =  tmp .. JAFDTC_SerializeObj(v, k, skipnewlines, depth + 1) .. "," .. (not skipnewlines and "\n" or "")
+        end
+
+        tmp = tmp .. string.rep(" ", depth) .. "}"
+    elseif type(val) == "number" then
+        tmp = tmp .. tostring(val)
+    elseif type(val) == "string" then
+        tmp = tmp .. string.format("%q", val)
+    elseif type(val) == "boolean" then
+        tmp = tmp .. (val and "true" or "false")
+    else
+        tmp = tmp .. "\"[inserializeable datatype:" .. type(val) .. "]\""
+    end
+
+    return tmp
+end
+
+function JAFDTC_LogSerializedObj(obj)
+    JAFDTC_Log(JAFDTC_SerializeObj(obj));
 end
 
 function JAFDTC_LinesToList(s)
@@ -80,19 +116,70 @@ function JAFDTC_SplitString(str, delimiter)
     return result
 end
 
-function JAFDTC_GetDisplay(id)
-    local SEP = "-----------------------------------------"
+-- notes on list_indication() output that provides display details:
+--
+-- output from this function is made up of a list of nodes where each node has a name (or key), a value, and one
+-- or more child nodes. names need not be unique across the hierarchy. for example,
+--
+--     a: value = "x"
+--        children = b : value = "y"
+--                   a : value = "w"
+--     c: value = "z"
+--
+-- here, we have a node "a" with value "x", and a node "c" with value "z". node "a" also has two children: nodes
+-- "b" and "a", with values "y" and "z" (respectively) and neither having any children of their own.
+--
+-- this output is parsed into a lua table with the JAFDTC_ParseDisplay() or JAFDTC_ParseDisplayFlat() functions
+-- that differ in how they express the hierarchy.
+--
+-- JAFDTC_ParseDisplay() ignores the hierarchy and returns the following table for the above example:
+--
+--     { "a" : "w", "b" : "y", c : "z" }
+--
+-- note that the value of key "a" in the table is the last node with that key, regardless of where it appears in
+-- the hierarchy.
+--
+-- JAFDTC_ParseDisplayFlat() flattens the hierarchy and would return the following table for the above example:
+--
+--     { "a.1" : "x", "a.2.b.1" : "y", "a.2.a.1" : "w", "c.1" : "z" }
+
+-- return a table representing a display. the table ignores hierarchy in the display representation.
+--
+function JAFDTC_ParseDisplaySimple(id)  -- Thanks to [FSF]Ian code
     local s = list_indication(id)
-    local err = false
     if not s then
         return { }
     end
-    local cur_table = { }
-    local tables = { cur_table }
+
+    local t = { }
+    local m = s:gmatch("-----------------------------------------\n([^\n]*)\n([^\n]*)\n")
+    while true do
+        local name, value = m()
+        if not name then
+            break
+        elseif string.len(name) > 0 then
+            t[name] = value
+        end
+    end
+    return t
+end
+
+-- return a table representing a display. the table flattens hierarchy in the display representation.
+--
+function JAFDTC_ParseDisplayFlat(id)
+    local s = list_indication(id)
+    if not s then
+        return { }
+    end
     local strlist = JAFDTC_LinesToList(s)
     if #strlist == 0 then
         return { }
     end
+
+    local SEP = "-----------------------------------------"
+    local err = false
+    local cur_table = { }
+    local tables = { cur_table }
     local i = 1
     repeat
         if strlist[i] == SEP then
@@ -131,54 +218,17 @@ function JAFDTC_GetDisplay(id)
     return JAFDTC_FlattenTable(cur_table)
 end
 
--- TODO: do a table lookup here
-function JAFDTC_GetPlayerAircraftType()
-    local data = LoGetSelfData();
-    if data then
-        local model = string.upper(data["Name"])
-        if model == "AV8BNA" then return "AV8B" end
-        if model == "A-10C_2" then return "A10C" end
-        if model == "F-14A-135-GR" then return "F14AB" end
-        if model == "F-14B" then return "F14AB" end
-        if model == "F-15ESE" then return "F15E" end
-        if model == "F-16C_50" then return "F16CM" end
-        if model == "FA-18C_HORNET" then return "FA18C" end
-        if model == "M-2000C" then return "M2000C" end
-        return model;
-    end
-    return "Unknown"
-end
-
-function JAFDTC_SerializeDisplay(val, name, skipnewlines, depth)
-    skipnewlines = skipnewlines or false
-    depth = depth or 0
-
-    local tmp = string.rep(" ", depth)
-
-    if name then tmp = tmp .. "<" .. name .. "> = " end
-
-    if type(val) == "table" then
-        tmp = tmp .. "{" .. (not skipnewlines and "\n" or "")
-
-        for k, v in pairs(val) do
-            tmp =  tmp .. JAFDTC_SerializeDisplay(v, k, skipnewlines, depth + 1) .. "," .. (not skipnewlines and "\n" or "")
-        end
-
-        tmp = tmp .. string.rep(" ", depth) .. "}"
-    elseif type(val) == "number" then
-        tmp = tmp .. tostring(val)
-    elseif type(val) == "string" then
-        tmp = tmp .. string.format("%q", val)
-    elseif type(val) == "boolean" then
-        tmp = tmp .. (val and "true" or "false")
-    else
-        tmp = tmp .. "\"[inserializeable datatype:" .. type(val) .. "]\""
-    end
-
-    return tmp
-end
-
+-- TODO: deprecate
 function JAFDTC_DebugDisplay(display)
-    local tbl = JAFDTC_SerializeDisplay(display);
-    JAFDTC_Log(tbl);
+    JAFDTC_Log(JAFDTC_SerializeObject(display));
+end
+
+-- TODO: depreate
+function JAFDTC_ParseDisplay(id)
+    return JAFDTC_ParseDisplaySimple(id)
+end
+
+-- TODO: depreate
+function JAFDTC_GetDisplay(id)
+    return JAFDTC_ParseDisplayFlat(id)
 end
